c5cd469dc1c60cf5413f8de1708825cae941b511
gamjagoon <rlaalswo201@gmail.com>
2020-06-24 19:49:21 +0900

    cli cobra understand

---
+++ CLI_ToDo_Go/cmd/root.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+  Use : "Task",
+  Short : "Task is a CLI task manager",
+  // Run : func(cmd *cobra.Command, args[] string){
+
+  // },
+}
